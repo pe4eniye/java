a2c1a81de2a088f4706a3fbd99099a13618dcc0d
pe4eniye <pe4eniye@gmail.com>
2022-05-18 15:33:52 +0300

    1

--- test_project_1/src/main/java/packages/test_project_1/MyFirstProgram.java
+++ test_project_1/src/main/java/packages/test_project_1/MyFirstProgram.java
@@ -1,4 +1,6 @@
-public class myFirstProgram {
+package packages.test_project_1;
+
+public class MyFirstProgram {
 
 public static void main(String[] args) {
 	System.out.println ("How about them apples?");

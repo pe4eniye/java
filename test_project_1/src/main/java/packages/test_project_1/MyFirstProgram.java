191eb83f7c4adb8d6b1c5a84eb4beeea81a12973
pe4eniye <pe4eniye@gmail.com>
2022-05-20 18:01:46 +0300

    Поигрался с тестовойпрогой

--- test_project_1/src/main/java/packages/test_project_1/MyFirstProgram.java
+++ test_project_1/src/main/java/packages/test_project_1/MyFirstProgram.java
@@ -2,8 +2,17 @@ package packages.test_project_1;
 
 public class MyFirstProgram {
 
-  public static void main(String[] args) {
-    System.out.println("How about them apples?");
+  public static void main (String[] args) {
+
+    int a = 6;
+    int b = 6;
+    System.out.println("Площадь прямоугольника = " + Kvadrat(a, b));
+
+    }
+
+  public static int Kvadrat(int dlina, int shirina) {
+    return dlina * shirina;
   }
 
-}
+  }
+

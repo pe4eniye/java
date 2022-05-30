890f2cc4216c95345320e9dc4269d88379fae640
pe4eniye <pe4eniye@gmail.com>
2022-05-30 15:01:05 +0300

    ОТСЮДА СТАРТ нормального, а старое гавно

--- MyAutoTests/src/test/java/ui/tests/Test2.java
+++ MyAutoTests/src/test/java/ui/tests/Test2.java
@@ -2,9 +2,30 @@ package ui.tests;
 
 public class Test2 {
  public static void main(String[] args) {
+  String number = "30";
+  int wow123 = Integer.parseInt(number);
+  System.out.println(wow123);
+
+
+  System.out.println();
+  String woow = "Hi. My name is %s . I am %d years old.";
+  String name = "Petya";
+  int age = 32;
+  System.out.println(String.format(woow, name, age));
+
+  System.out.println();
+  int b = 5;
+  System.out.println(!(b > 10));
+  System.out.println();
+
+  String yurii = "Сравним строку!";
+  String yurii222 = "С этой строкой. Будет Фолс, так как текст отличается";
+  System.out.println(yurii.equals(yurii222));
+  System.out.println();
 
   int[] yurec = {1, 2, 3, 4, 5, 6};
           for (int i=0; i<6; i++)
            System.out.println(i);
+
  }
 }
